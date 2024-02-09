1699054e0bd228578f5bf91521fb810aa10fcb1a
Abigirl Muchineripi <muchineripiabby@gmail.com>
2024-02-09 06:34:11 +0200

    script.js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -11,12 +11,19 @@ const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 const monsterHealthText = document.querySelector("#monsterHealth");
 const locations = [
-    { name:"town square"},
+    { 
+    name:"town square",    
     "button text":[        
             "Go to store",
             "Go to cave",
             "Fight dragon"        
-    ]
+    ],
+    "button functions" :[
+        goStore, goCave, fightDragon
+      ],
+      "text":"You are in the town square. You see a sign that says \"Store\"."
+ 
+    }
 ];
 //initialize buttons
 button1.onclick = goStore;
