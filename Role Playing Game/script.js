aa63784e7bbd54358124d0b53a0dea190498ac63
Abigirl Muchineripi <muchineripiabby@gmail.com>
2024-02-19 09:38:52 +0200

    script.js

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -135,9 +135,18 @@ function buyWeapon() {
         text.innerText = "You already have the most powerful weapon!";
         button2.innerText ="Sell weapon for 15 gold";
         button2.onclick = sellWeapon;
-      }
-    
+      }    
   }
+
+function sellWeapon () {
+  if( inventory.length > 1){
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+
+    
+  }  
+}
 function fightSlime(){
 
 }
